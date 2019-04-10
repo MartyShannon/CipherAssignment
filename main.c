ec72351caf527132d1c5781e32e6df445896dce1
Marty Shannon <themartyshannon@gmail.com>
2019-04-10 07:39:57 +0000

    Added switch case and menu, still trying to get encoder to work as a function

--- main.c
+++ main.c
@@ -1,71 +1,61 @@
 #include <stdio.h>
 #include <string.h>
 
-void encoder(char str[], int i, int r);
+char rotationEncoder(char string[]);
 
 int main()
 {
-   char string[100];
-   int rotation = 0;
-   int index = 0;
-   
-   printf("Text you would like to encode, All in caps and no spaces:");
-   gets(string);
-   //scanf("%s", &string[0]);     //%s scans for a string variable, not sure about white space though and how to avoid
-   //printf("%s\n", &string[0]);        prints original input for testing
-   
-   printf("Now enter a value you would like the cipher to rotate the alphabet by (Number 1-25):");
-   scanf("%d", &rotation);
-   
-   
-   // THIS CODE WORKS, trying to make into a function
-   while(string[index] != 0)
-   {
-       string[index] += rotation;   //This moves all letters up by the designated rotation amount.
+    int f = 0;
+    
+    printf("Type number for desired function:\n   1 -- Rotation Cypher Encryption\n   2 -- Rotation Cypher Decrytion\n   3 -- Substitution Cypher Enryption\n   4 -- Substitution Cypher Decyption\n   5 -- Decryption of Rotation Cypher (Rotation unknown)\n   6 -- Decrytion of a Substitution Cypher (substitution unkown)\n");
+    scanf("%d", &f);
+    
+    switch(f) 
+    {
+        case 1:
+        {
+            char string[100];
+            
+            printf("Enter message, all in caps: ");
+            scanf("%s", &string[0]);
+            char roatationEncoder(char *string);
+            printf("%s\n", &string[0]);
+            
+            break;
+        }
 
-       //if statement says: if string number greater than 90 it will minus 26 ie. send the letters at the end of the alphabet to the start.
-       if(string[index] > 90)
-       {
-           string[index] -= 26;
-       }
-       
-       //for(string[index] == 35; string[index] -= rotation;)      Causes segmentation error, but would be a neater way of laying out then a if statement.
-       
-       
-       //if statement says: if string number equal to 32 it undoes the rotation ie. so spaces stay as spaces.
-       if(string[index] == 32 + rotation)
-       {
-           string[index] -= rotation;
-       }  
-       index++; //increments value to perform loop for string
+        case 2: 
+            break;
+        case 3: 
+            break;
+        default:
+            printf("You only have options 1 to 6\n");
+
     }
-
-   //encoder(char string[], int index, int rotation);  
-   printf("%s\n", string);
-  
-  
-   return 0;
+    
+    return 0;
 }
 
-/* Attempt at turning working code into a function
-void encoder(char str[], int i, int r)   
+char rotationEncoder(char string[])
 {
-    while(str[i] != 0)
-   {
-       str[i] += r;
+    int rotation = 10;
+    int index = 0;
 
-       if(str[i] > 90)
-       {
-           str[i] -= 26;
-       }
-       
-       //for(string[index] == 35; string[index] -= rotation;)      Causes segmentation error, but would be a neater way of laying out then a if statement.
-       
-       if(str[i] == 32 + r)
-       {
-           str[i] -= r;
-       }
-    }
+    while(string[index] != 0)
+    {
+        string[index] += rotation; //This moves all letters up by the designated rotation amount.
 
-    return char str[];
-}*/
+        if(string[index] > 90) //if string number greater than 90 it will minus 26 ie. send the letters at the end of the alphabet to the start.
+        {
+            string[index] -= 26;
+        }
+       
+        if(string[index] == 32 + rotation) //if statement says: if string number equal to 32 it undoes the rotation ie. so spaces stay as spaces.
+        {
+            string[index] -= rotation;
+        }  
+        index++; //increments value to perform loop for string
+     }
+     //printf("%s\n", string);
+     return;
+}
